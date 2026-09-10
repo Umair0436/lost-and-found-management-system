f2cbdd5f0c0ad99a14134effcce1a289a5ae7997
Umair0436 <Umair00436@gmail.com>
2026-09-10 13:44:08 +0500

    Implement user authentication with Devise and enhance item management features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_07_070547) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_09_081647) do
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -18,5 +18,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_07_070547) do
     t.string "name"
     t.string "status"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
